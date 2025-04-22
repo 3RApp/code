bb72b0323081ca781c58bfb0b731b15c7ad94658
Kuzmin Anton <antofer@mail.ru>
2025-04-22 20:34:02 +0300

    Вынесены разделы меню

--- 6/6.1/src/App.js
+++ 6/6.1/src/App.js
@@ -2,13 +2,14 @@ import {useState} from'react';
 import { Contacts, Books, Order } from "./business";
 import { Footer, Header } from './components';
 import './App.css';
+import { menuTitleList } from './menuTitleList';
 
 function App() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
     <div className="App">
-      <Header list={["Книги", "Заказ", "Контакты"]} cb={setCurrentIndex} />
+      <Header list={menuTitleList} cb={setCurrentIndex} />
       {
         [Books, Order, Contacts].map((Page, index) => <Page key={index} className={currentIndex !== index ? 'hidden' : ''}  />)
       }
---
+++ 6/6.1/src/menuTitleList.js
@@ -0,0 +1,1 @@
+export const menuTitleList = ["Книги", "Заказ", "Контакты"];
